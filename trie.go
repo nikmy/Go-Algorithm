e03d7b672a2711785d429ea70dba9a4d5855d040
Nikita Mysov <70437822+My-Ink@users.noreply.github.com>
2022-03-04 00:29:20 +0300

    Create trie.go

---
+++ trie.go
@@ -0,0 +1,57 @@
+// Trie
+/////////////////////////////////////////////////////////////////////////////////////////
+type node struct {
+  next    map[uint8]int
+  leaf_id int
+}
+
+func newNode(leaf_id int) node {
+  return node{make(map[uint8]int), leaf_id}
+}
+
+type Trie struct {
+  tree []node
+}
+
+func NewTrie() *Trie {
+  return &Trie{[]node{newNode(-1)}}
+}
+
+func (t *Trie) TryNext(n int, c uint8) bool {
+  _, can := t.tree[n].next[c]
+  return can
+}
+
+func (t *Trie) GoNext(n int, c uint8) int {
+  child, _ := t.tree[n].next[c]
+  return child
+}
+
+func (t *Trie) Insert(id int, s string) {
+  n := 0
+  for i := 0; i < len(s); i++ {
+    if t.TryNext(n, s[i]) {
+      n = t.GoNext(n, s[i])
+    } else {
+      t.tree[n].next[s[i]] = len(t.tree)
+      if i != len(s) - 1 {
+        n, t.tree = len(t.tree), append(t.tree, newNode(-1))  
+      } else {
+        t.tree = append(t.tree, newNode(id))
+      }
+    }
+  }
+}
+
+func (t *Trie) Find(s string) int {
+  n := 0
+  for i := 0; i < len(s); i++ {
+    if t.TryNext(n, s[i]) {
+      n = t.GoNext(n, s[i])
+    } else {
+      return -1
+    }
+  }
+  return t.tree[n].leaf_id
+}
+/////////////////////////////////////////////////////////////////////////////////////////
