9a86498ea4e8aff6d568c4e32e5adab29a904722
Nikita Mysov <dev.myink@gmail.com>
2025-08-11 11:30:42 +0300

    generic

---
+++ container/lockfree/skiplist/generic.go
@@ -0,0 +1,280 @@
+package skiplist
+
+import (
+	"cmp"
+	"fmt"
+	"math/rand/v2"
+	"strings"
+	"sync/atomic"
+)
+
+func NewGeneric[T cmp.Ordered]() *GenericList[T] {
+	stub := &genericTower[T]{
+		next: make([]atomic.Pointer[genericTower[T]], maxLevel),
+	}
+	stub.state.Store(towerStateDeleting)
+	return &GenericList[T]{
+		leftmost: stub,
+	}
+}
+
+func MakeGeneric[T cmp.Ordered](values ...T) *GenericList[T] {
+	l := NewGeneric[T]()
+	for _, v := range values {
+		l.Insert(v)
+	}
+	return l
+}
+
+type GenericList[T cmp.Ordered] struct {
+	leftmost *genericTower[T]
+}
+
+func (l *GenericList[T]) Find(x T) bool {
+	return l.leftmost.find(x)
+}
+
+func (l *GenericList[T]) Insert(x T) bool {
+	var linksToUpdate [maxLevel]*genericTower[T]
+	n, found := l.leftmost.findLinks(linksToUpdate[:], x)
+	if found != nil {
+		return false
+	}
+	return newGenericTower[T](x).link(linksToUpdate[:n])
+}
+
+func (l *GenericList[T]) Delete(x T) bool {
+	var linksToUpdate [maxLevel]*genericTower[T]
+	n, target := l.leftmost.findLinks(linksToUpdate[:], x)
+	if target == nil {
+		return false
+	}
+	return target.unlink(linksToUpdate[:n])
+}
+
+func (l *GenericList[T]) Elements(yield func(T) bool) {
+	base := l.leftmost.next[0].Load()
+	for node := base; node != nil; node = node.next[0].Load() {
+		if !yield(node.elem) {
+			break
+		}
+	}
+}
+
+func (l *GenericList[T]) IsEmpty() bool {
+	return l.leftmost.next[0].Load() == nil
+}
+
+// String formats elements like a slice.
+func (l *GenericList[T]) String() string {
+	if l == nil || l.leftmost == nil {
+		return "<nil>"
+	}
+	if l.leftmost.next[0].Load() == nil {
+		return "[]"
+	}
+	elements := make([]string, 0)
+	for elem := range l.Elements {
+		elements = append(elements, fmt.Sprintf("%v", elem))
+	}
+	return "[" + strings.Join(elements, ", ") + "]"
+}
+
+// SDump is debug only. Dumps skip list in the following format:
+//
+//	[head] -> 1 --------------> 3 --------------> [end]
+//	[head] -> 1 --------------> 3 -----> 4 -----> [end]
+//	[head] -> 1 -----> 2 -----> 3 -----> 4 -----> [end]
+func (l *GenericList[T]) SDump() string {
+	if l == nil || l.leftmost == nil {
+		return "<nil>"
+	}
+
+	if l.leftmost.next[0].Load() == nil {
+		return "[]"
+	}
+
+	levels := make([][]T, 0)
+	maxWidth := 0
+	for lvl := range maxLevel {
+		if l.leftmost.next[lvl].Load() == nil {
+			break
+		}
+
+		level := make([]T, 0)
+		for node := l.leftmost.next[lvl].Load(); node != nil; node = node.next[lvl].Load() {
+			level = append(level, node.elem)
+			maxWidth = max(maxWidth, len(fmt.Sprintf("%v", node.elem)))
+		}
+		levels = append(levels, level)
+	}
+
+	var sb strings.Builder
+	for k := len(levels) - 1; k >= 0; k-- {
+		level := levels[k]
+
+		sb.WriteString("[head] -")
+		i := 0
+		for _, elem := range levels[0] {
+			if i < len(level) && elem == level[i] {
+				s := fmt.Sprintf("%v", elem)
+				sb.WriteString("> [")
+				sb.WriteString(s)
+				sb.WriteString("] ")
+				sb.WriteString(strings.Repeat("-", maxWidth-len(s)))
+				i++
+			} else {
+				sb.WriteString(strings.Repeat("-", maxWidth+5))
+			}
+			sb.WriteString("-")
+		}
+		sb.WriteString("> [end]\n")
+	}
+	return sb.String()
+}
+
+func newGenericTower[T cmp.Ordered](x T) *genericTower[T] {
+	levels := 1
+	for levels < maxLevel && rand.Int()%4 == 0 {
+		levels++
+	}
+
+	return &genericTower[T]{
+		elem: x,
+		next: make([]atomic.Pointer[genericTower[T]], levels),
+	}
+}
+
+type genericTower[T cmp.Ordered] struct {
+	elem T
+	next []atomic.Pointer[genericTower[T]]
+
+	state atomic.Int32
+}
+
+func (t *genericTower[T]) find(x T) bool {
+	node := t
+
+	for level := len(t.next) - 1; level >= 0; level-- {
+		for node != nil && (node.state.Load() == towerStateDeleting || node.elem < x) {
+			next := node.next[level].Load()
+			if next == nil || next.elem > x {
+				break
+			}
+			node = next
+		}
+		if node == nil {
+			return false
+		}
+		if node.state.Load() != towerStateDeleting && node.elem == x {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (t *genericTower[T]) findLinks(links []*genericTower[T], x T) (int, *genericTower[T]) {
+	var (
+		node = t
+		next *genericTower[T]
+	)
+	for level := len(t.next) - 1; level >= 0; level-- {
+		next = node.next[level].Load()
+		for next != nil && next.elem < x {
+			node = next
+			next = next.next[level].Load()
+		}
+		links[level] = node
+	}
+
+	if next != nil && next.state.Load() != towerStateDeleting && next.elem == x {
+		return len(t.next), next
+	}
+	return len(t.next), nil
+}
+
+func (t *genericTower[T]) link(links []*genericTower[T]) bool {
+	for level := 0; level < len(t.next); level++ {
+		left := links[level]
+		for {
+			right := left.next[level].Load()
+			for right != nil && right.elem < t.elem {
+				left = right
+				right = right.next[level].Load()
+			}
+			if level == 0 && right != nil && right.elem == t.elem {
+				return false
+			}
+
+			t.next[level].Store(right)
+			if left.next[level].CompareAndSwap(right, t) {
+				break
+			}
+		}
+	}
+
+	t.state.Store(towerStateCreated)
+
+	return true
+}
+
+func (t *genericTower[T]) unlink(links []*genericTower[T]) bool {
+	if !t.state.CompareAndSwap(towerStateCreated, towerStateDeleting) {
+		return false
+	}
+
+	for level := 0; level < len(t.next); level++ {
+		/*
+			Unlinking B from A -> B -> C
+
+			1. Make loop:
+
+				A -> (B)
+
+			2. Switch forward link:
+
+				A   (B)   C
+			    |---------^
+
+			3. Make reverse link:
+
+				A <--- B    C
+				|-----------^
+		*/
+
+		// Step 1: announce deletion (make a loop link)
+		var right *genericTower[T]
+		for {
+			right = t.next[level].Load()
+			if t.next[level].CompareAndSwap(right, t) {
+				break
+			}
+		}
+
+		// Step 2: switch forward link
+		left := links[level]
+		for {
+			next := left.next[level].Load()
+			for next != nil && next.elem < t.elem {
+				left = next
+				next = left.next[level].Load()
+			}
+			if next == nil || next.elem > t.elem {
+				break
+			}
+			if left.next[level].CompareAndSwap(t, right) {
+				break
+			}
+		}
+
+		// Step 3: make reverse link
+		if t.next[level].CompareAndSwap(t, left) {
+			if t.next[level].Load() != left {
+				panic("genericTower[T] unlink fail")
+			}
+		}
+	}
+
+	return true
+}
---
+++ container/lockfree/skiplist/generic_test.go
@@ -0,0 +1,519 @@
+package skiplist
+
+import (
+	"context"
+	"runtime"
+	"slices"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestList_Find(t *testing.T) {
+	t.Parallel()
+
+	tests := [...]struct {
+		name string
+		list *GenericList[int64]
+		elem int64
+		want bool
+	}{
+		{
+			name: "found max",
+			list: MakeGeneric[int64](generateInts(1, 9, 1)...),
+			elem: 8,
+			want: true,
+		},
+		{
+			name: "found min",
+			list: MakeGeneric[int64](generateInts(1, 9, 1)...),
+			elem: 1,
+			want: true,
+		},
+		{
+			name: "found middle",
+			list: MakeGeneric[int64](generateInts(1, 9, 1)...),
+			elem: 4,
+			want: true,
+		},
+		{
+			name: "not found",
+			list: MakeGeneric[int64](generateInts(1, 9, 1)...),
+			elem: 9,
+			want: false,
+		},
+		{
+			name: "found negative",
+			list: MakeGeneric[int64](1, -2, -5, 7, -8),
+			elem: -5,
+			want: true,
+		},
+		{
+			name: "not found negative",
+			list: MakeGeneric[int64](1, -2, -5, 7, -8),
+			elem: -4,
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Logf("dump:\n%s\n", tt.list.SDump())
+			if got := tt.list.Find(tt.elem); got != tt.want {
+				t.Errorf("List.Find(%d) = %v, want %v", tt.elem, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestList_Insert(t *testing.T) {
+	t.Parallel()
+
+	l := MakeGeneric[int64](1, 2, 3)
+	if !l.Insert(4) {
+		t.Error("cannot insert new element: expected true, got false")
+	}
+	if l.Insert(3) {
+		t.Error("double insertion: expected false, got true")
+	}
+}
+
+func TestList_Delete(t *testing.T) {
+	t.Parallel()
+
+	l := MakeGeneric[int64](1, 2, 3)
+	if l.Delete(4) {
+		t.Errorf("deleted element that does not exist")
+	}
+	if !l.Delete(1) {
+		t.Errorf("cannot delete element that exists")
+	}
+	if l.Delete(1) {
+		t.Errorf("double deleted element")
+	}
+}
+
+func TestList_Elements(t *testing.T) {
+	t.Parallel()
+
+	t.Run("empty list", func(t *testing.T) {
+		list := NewGeneric[int64]()
+		for elem := range list.Elements {
+			t.Errorf("Unexpected element %d", elem)
+		}
+	})
+
+	t.Run("traversal", func(t *testing.T) {
+		l := MakeGeneric[int64](generateInts(1, 11, 1)...)
+		want := int64(1)
+		for elem := range l.Elements {
+			if elem != want {
+				t.Errorf("List.Elements() = %v, want %v", elem, want)
+			}
+			want++
+		}
+	})
+}
+
+func TestList_Visual(t *testing.T) {
+	t.Parallel()
+
+	tests := [...]struct {
+		name string
+		list *GenericList[int64]
+	}{
+		{
+			name: "empty list",
+			list: NewGeneric[int64](),
+		},
+		{
+			name: "one element",
+			list: MakeGeneric[int64](42),
+		},
+		{
+			name: "small",
+			list: MakeGeneric[int64](generateInts(6, 0, -1)...),
+		},
+		{
+			name: "medium",
+			list: MakeGeneric[int64](generateInts(-1, 11, 1)...),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Logf("fmt: %s\n", tt.list)
+			t.Logf("dump:\n%s\n", tt.list.SDump())
+		})
+	}
+}
+
+func TestList_IsEmpty(t *testing.T) {
+	t.Parallel()
+
+	list := NewGeneric[int64]()
+	assert.True(t, list.IsEmpty())
+
+	list.Insert(1)
+	assert.False(t, list.IsEmpty())
+}
+
+func TestList_RaceFree(t *testing.T) {
+	t.Run("find-find", func(t *testing.T) {
+		ctrl := newCtrl(t)
+		list := MakeGeneric[int64](generateInts(2, 11, 2)...)
+		for i := int64(2); i <= 10; i += 2 {
+			ctrl.Spawn(200, func() {
+				assert.Truef(t, list.Find(i), "missing element %d", i)
+			})
+		}
+		for i := int64(1); i <= 9; i += 2 {
+			ctrl.Spawn(200, func() { assert.Falsef(t, list.Find(i), "unexpected element %d", i) })
+		}
+
+		ctrl.Run(5 * time.Second)
+	})
+
+	t.Run("insert-insert", func(t *testing.T) {
+		ctrl := newCtrl(t)
+		list := NewGeneric[int64]()
+		inserted := [50]atomic.Int64{}
+		for i := int64(0); i < 50; i++ {
+			ctrl.Spawn(400, func() {
+				ok := list.Insert(i)
+				if ok {
+					n := int(inserted[i].Add(1))
+					assert.Lessf(t, n, 2, "multiple insertion: %d", n)
+				}
+			})
+		}
+
+		ctrl.Run(5 * time.Second)
+
+		assert.Equal(t, slices.Collect(list.Elements), generateInts(0, 50, 1))
+
+		t.Logf("list after all operations:\n%s\n", list.SDump())
+	})
+
+	t.Run("find-insert", func(t *testing.T) {
+		ctrl := newCtrl(t)
+		list := NewGeneric[int64]()
+		inserted := [50]atomic.Int64{}
+		for i := int64(0); i < 50; i++ {
+			ctrl.Spawn(400, func() {
+				_ = list.Find(i)
+
+				ok := list.Insert(i)
+				if ok {
+					n := int(inserted[i].Add(1))
+					assert.Lessf(t, n, 2, "multiple insertion: %d", n)
+				}
+
+				ok = list.Find(i)
+				assert.Truef(t, ok, "can't found element %d after insertion", i)
+			})
+		}
+
+		ctrl.Run(5 * time.Second)
+
+		t.Logf("list after all operations:\n%s\n", list.SDump())
+	})
+
+	t.Run("delete-delete", func(t *testing.T) {
+		ctrl := newCtrl(t)
+		list := MakeGeneric[int64](generateInts(0, 60, 1)...)
+
+		t.Logf("list before all operations:\n%s\n", list.SDump())
+
+		for i := int64(0); i < 60; i++ {
+			ctrl.Spawn(400, func() {
+				_ = list.Delete(i * 2)
+				ok := list.Delete(i * 2)
+				if ok {
+					assert.Failf(t, "double delete", "element %d deleted after delete", i*2)
+				}
+
+				_ = list.Delete(i*2 + 1)
+				ok = list.Delete(i*2 + 1)
+				if ok {
+					assert.Failf(t, "double delete", "element %d deleted after delete", i*2+1)
+				}
+			})
+		}
+
+		ctrl.Run(5 * time.Second)
+
+		t.Logf("list after all operations:\n%s\n", list.SDump())
+	})
+
+	t.Run("find-delete", func(t *testing.T) {
+		ctrl := newCtrl(t)
+		list := MakeGeneric[int64](generateInts(0, 40, 2)...)
+
+		t.Logf("list before all operations:\n%s\n", list.SDump())
+
+		deleted := [50]atomic.Int64{}
+		for i := int64(0); i < 50; i++ {
+			ctrl.Spawn(400, func() {
+				_ = list.Find(i * 2)
+
+				ok := list.Delete(i*2 + 1)
+				assert.Falsef(t, ok, "deleted element %d that does not exist", i*2+1)
+
+				ok = list.Delete(i * 2)
+				if ok {
+					n := int(deleted[i].Add(1))
+					assert.Lessf(t, n, 2, "multiple delete of %d: %d", i*2, n)
+				}
+
+				if ok {
+					assert.Falsef(t, list.Find(i*2), "element %d found after deletion", i*2)
+				}
+			})
+		}
+
+		ctrl.Run(5 * time.Second)
+
+		t.Logf("list after all operations:\n%s\n", list.SDump())
+	})
+
+	t.Run("insert-delete", func(t *testing.T) {
+		ctrl := newCtrl(t)
+		list := NewGeneric[int64]()
+
+		t.Logf("list before all operations:\n%s\n", list.SDump())
+
+		counts := [100][2]atomic.Int64{}
+		inc := func(i int64, b bool) {
+			if b {
+				counts[i][0].Add(1)
+			}
+		}
+		dec := func(i int64, b bool) {
+			if b {
+				counts[i][1].Add(1)
+			}
+		}
+
+		for i := int64(0); i < 50; i++ {
+			ctrl.Spawn(10, func() {
+				inc(i*2+1, list.Insert(i*2+1))
+				inc(i*2, list.Insert(i*2))
+				dec(i*2, list.Delete(i*2))
+				dec(i*2+1, list.Delete(i*2+1))
+			})
+		}
+
+		ctrl.Run(5 * time.Second)
+
+		for i := range counts {
+			inserts := counts[i][0].Load()
+			deletes := counts[i][1].Load()
+			assert.Equal(t, inserts, deletes, "inserts (expected) and deletes (actual) disbalanced")
+			assert.Greaterf(t, inserts, int64(0), "no successful inserts / deletes")
+		}
+
+		assert.Len(t, slices.Collect(list.Elements), 0, "list must be empty")
+
+		t.Logf("list after all operations:\n%s\n", list.SDump())
+	})
+}
+
+func TestList_Serializability(t *testing.T) {
+	t.Parallel()
+
+	t.Run("insert", func(t *testing.T) {
+		ctrl := newCtrl(t)
+
+		list := NewGeneric[int64]()
+		inserted := [51]atomic.Int64{}
+		for i := int64(0); i < 50; i++ {
+			ctrl.Spawn(100, func() {
+				if list.Insert(i) {
+					inserted[i].Add(1)
+				}
+				if list.Insert(i + 1) {
+					inserted[i+1].Add(1)
+				}
+			})
+		}
+
+		ctrl.Run(5 * time.Second)
+
+		for i := range inserted {
+			assert.EqualValues(t, 1, inserted[i].Load(), "element %d is not inserted once", i)
+		}
+	})
+
+	t.Run("delete", func(t *testing.T) {
+		ctrl := newCtrl(t)
+
+		list := MakeGeneric[int64](generateInts(0, 51, 1)...)
+		deleted := [51]atomic.Int64{}
+
+		for i := int64(0); i < 50; i++ {
+			ctrl.Spawn(100, func() {
+				if list.Delete(i) {
+					deleted[i].Add(1)
+				}
+				if list.Delete(i + 1) {
+					deleted[i+1].Add(1)
+				}
+			})
+		}
+
+		ctrl.Run(5 * time.Second)
+
+		for i := range deleted {
+			assert.EqualValues(t, 1, deleted[i].Load(), "element %d is not deleted once", i)
+		}
+	})
+
+	t.Run("insert and delete", func(t *testing.T) {
+		ctrl := newCtrl(t)
+
+		list := NewGeneric[int64]()
+		inserted := [51]atomic.Int64{}
+		deleted := [51]atomic.Int64{}
+		for i := int64(0); i < 50; i++ {
+			ctrl.Spawn(100, func() {
+				if list.Insert(i) {
+					inserted[i].Add(1)
+				}
+				if list.Delete(i) {
+					deleted[i].Add(1)
+				}
+			})
+		}
+
+		ctrl.Run(5 * time.Second)
+
+		for i := range len(inserted) {
+			assert.EqualValues(t, inserted[i].Load(), deleted[i].Load(), "inserted (expected) != deleted (actual)", i)
+		}
+	})
+
+	t.Run("delete and insert", func(t *testing.T) {
+		ctrl := newCtrl(t)
+
+		list := MakeGeneric[int64](generateInts(0, 51, 1)...)
+		inserted := [51]atomic.Int64{}
+		deleted := [51]atomic.Int64{}
+		for i := int64(0); i < 50; i++ {
+			ctrl.Spawn(100, func() {
+				if list.Delete(i) {
+					deleted[i].Add(1)
+				}
+				if list.Insert(i) {
+					inserted[i].Add(1)
+				}
+			})
+		}
+
+		ctrl.Run(5 * time.Second)
+
+		for i := range len(inserted) {
+			assert.EqualValues(t, deleted[i].Load(), inserted[i].Load(), "inserted (expected) != deleted (actual)", i)
+		}
+	})
+
+	t.Run("insert and find", func(t *testing.T) {
+		ctrl := newCtrl(t)
+
+		list := NewGeneric[int64]()
+		for i := int64(0); i < 50; i++ {
+			ctrl.Spawn(100, func() {
+				_ = list.Insert(i)
+				assert.True(t, list.Find(i))
+			})
+		}
+
+		ctrl.Run(5 * time.Second)
+	})
+
+	t.Run("delete and find", func(t *testing.T) {
+		ctrl := newCtrl(t)
+
+		list := MakeGeneric[int64](generateInts(0, 51, 1)...)
+		for i := int64(0); i < 50; i++ {
+			ctrl.Spawn(100, func() {
+				if list.Delete(i) {
+					assert.False(t, list.Find(i))
+				}
+			})
+		}
+
+		ctrl.Run(5 * time.Second)
+	})
+
+	t.Run("delete eventual consistency", func(t *testing.T) {
+		ctrl := newCtrl(t)
+
+		list := MakeGeneric[int64](generateInts(0, 51, 1)...)
+		for i := int64(0); i < 50; i++ {
+			ctrl.Spawn(100, func() {
+				_ = list.Delete(i)
+				assert.Eventually(t, func() bool { return !list.Find(i) }, time.Second, 100*time.Millisecond)
+			})
+		}
+
+		ctrl.Run(5 * time.Second)
+	})
+}
+
+func generateInts(start, end, step int) []int64 {
+	ints := make([]int64, 0, (end-start)/step+1)
+	for i := start; i < end; i += step {
+		ints = append(ints, int64(i))
+	}
+	return ints
+}
+
+func newCtrl(t *testing.T) *pController {
+	return &pController{
+		t:  t,
+		do: make(chan struct{}),
+	}
+}
+
+type pController struct {
+	t  *testing.T
+	wg sync.WaitGroup
+	do chan struct{}
+	n  atomic.Int64
+}
+
+func (c *pController) Run(timeout time.Duration) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	close(c.do)
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+		c.wg.Wait()
+	}()
+	select {
+	case <-done:
+	case <-ctx.Done():
+		stack := make([]byte, 100_000)
+		n := runtime.Stack(stack, true)
+		assert.Failf(c.t, "timed out", "%d goroutines stuck:\n--- stacktrace ---\n%s", c.n.Load(), stack[:n])
+	}
+}
+
+func (c *pController) Spawn(n int, g func()) {
+	c.wg.Add(n)
+	c.n.Add(int64(n))
+	for i := 0; i < n; i++ {
+		go func() {
+			defer c.wg.Done()
+			<-c.do
+			g()
+			c.n.Add(-1)
+		}()
+	}
+}
